67d2b6a9e695e94f4e878da3a5c53fd46734074f
Nadezhda Shumilova <nschumilova@gmail.com>
2017-12-12 13:42:31 +0300

    method added : checkMoreOrEqualZero

--- src/main/java/com/alds/util/Helper.java
+++ src/main/java/com/alds/util/Helper.java
@@ -6,12 +6,18 @@ public class Helper {
         if (input == null)
             throw new IllegalArgumentException("Input is null");
     }
+
     public static void checkNotEmpty(String string) {
-        if(string.trim().isEmpty())
+        if (string.trim().isEmpty())
             throw new IllegalArgumentException("Input string is empty");
     }
 
-    public static boolean sameObjects(String str1, String str2){
-        return str1==str2;
+    public static boolean sameObjects(String str1, String str2) {
+        return str1 == str2;
+    }
+
+    public static void checkMoreOrEqualZero(int value) {
+        if (value < 0)
+            throw new IllegalArgumentException("Input parameter(" + value + ") must be >=0");
     }
 }
